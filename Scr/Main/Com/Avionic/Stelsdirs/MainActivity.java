e25aae2179a3b7f59ff9b443abcad7fa8c4a3277
Avionic214 <diamian1100@gmail.com>
2025-03-15 23:38:16 +0300

    Update MainActivity.java

--- Scr/Main/Com/Avionic/Stelsdirs/MainActivity.java
+++ Scr/Main/Com/Avionic/Stelsdirs/MainActivity.java
@@ -1,19 +1,38 @@
+package com.avionic.fileTest;
+
 import android.app.*;
 import android.os.*;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Button;
 import android.widget.AdapterView;
+import android.content.Intent;
+import android.widget.ListView; 
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList; 
+import java.util.HashMap; 
+import java.util.Map; 
 
 import java.io.File;
 import java.nio.file.*;
+import android.widget.*;
+import android.graphics.drawable.*;
 
 public class MainActivity extends Activity 
 {
+	String root= Environment.getExternalStorageDirectory().getPath().toString();
 	File dir = new File(Environment.getExternalStorageDirectory().getPath());
 	String[] files = dir.list();
 	String[] files2;
+	String NSDir;
+	File ndir;
+	// имена атрибутов для Map 
+	final String ATTRIBUTE_NAME_TEXT = "text"; 
+	final String ATTRIBUTE_NAME_IMAGE = "image"; 
+	ListView lvSimple;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,40 +40,59 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
-		// получаем элемент ListView
-        ListView fileList = findViewById(R.id.dirs);
+		TextView dir_tv = findViewById(R.id.nfold);
+		Button but_up = findViewById(R.id.up);
+		dir_tv.setText(root);
+		int file_img=R.drawable.file;
+		int fold_img= R.drawable.folder;
+		int img;
 		
+		but_up.setBackgroundResource(R.drawable.updir);
 		
-		//File ren_dir = new File("/storage/emulated/0/test_file");
-		//File new_dir = new File("/storage/emulated/0/.test_file");
-		//ren_dir.renameTo(new_dir);
-		
-
-		// если доступ к содержимому папки запрещён - она вернёт null
-		// обязательно проверяем! 
         if (files != null)
-        {ArrayAdapter<String> adapter = new ArrayAdapter(this,
-			android.R.layout.simple_list_item_1, files);
-			
-			fileList.setAdapter(adapter);}
-        // устанавливаем для списка адаптер
-		// добавляем для списка слушатель
-        fileList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+		{ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>( 
+				files.length); 
+			Map<String, Object> m; 
+			for (int i = 0; i < files.length; i++) { 
+				m = new HashMap<String, Object>(); 
+				m.put(ATTRIBUTE_NAME_TEXT, files[i]);
+				ndir= new File(root+ "/"+files[i]);
+				if (ndir.isDirectory()){
+					img= fold_img;}
+					else{img=file_img;}
+				m.put(ATTRIBUTE_NAME_IMAGE, img); 
+				data.add(m);
+				}
+		
+		// массив имен атрибутов, из которых будут читаться данные 
+		String[] from = { ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_IMAGE }; 
+		// массив ID View-компонентов, в которые будут вставлять данные 
+		int[] to = { R.id.namef, R.id.ico }; 
+// создаем адаптер 
+		SimpleAdapter sAdapter = new SimpleAdapter(this, data,
+									R.layout.itemdir, from, to); 
+// определяем список и присваиваем ему адаптер 
+		lvSimple = findViewById(R.id.lvSimple); 
+		lvSimple.setAdapter(sAdapter);}
+		lvSimple.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 				@Override
 				public void onItemClick(AdapterView<?> parent, View v, int position, long id)
 				{
 					// по позиции получаем выбранный элемент
-					String selectedDir = files[position];
-					File new_dir= new File(selectedDir);
-					String[] files2=new_dir.list();
+					NSDir = files[position];
+					newDir();
 				}
-		});
-    }
+			});
+	}
+				
+        
+		
+    
 public void newDir(){
-	if (files2 != null)
-	{ArrayAdapter<String> adapter2 = new ArrayAdapter(this,
-	 android.R.layout.simple_list_item_1, files2);
-	fileList()
-}
+		Intent intent = new Intent(this, NewDir.class);
+		intent.putExtra("dir", dir.toString());
+		intent.putExtra("file", NSDir);
+		startActivity(intent); }
 	
 }
+	
