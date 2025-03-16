4a0b889cc9605b144d80e6c9c9b5f9ddef855e62
Avionic214 <diamian1100@gmail.com>
2025-03-16 18:45:06 +0300

    Add files via upload

--- Scr/Main/Com/Avionic/Stelsdirs/MainActivity.java
+++ Scr/Main/Com/Avionic/Stelsdirs/MainActivity.java
@@ -23,9 +23,9 @@ import android.graphics.drawable.*;
 
 public class MainActivity extends Activity 
 {
-	String root= Environment.getExternalStorageDirectory().getPath().toString();
-	File dir = new File(Environment.getExternalStorageDirectory().getPath());
-	String[] files = dir.list();
+	String o_dir;
+	File dir;
+	String[] files;
 	String[] files2;
 	String NSDir;
 	File ndir;
@@ -34,15 +34,21 @@ public class MainActivity extends Activity
 	final String ATTRIBUTE_NAME_IMAGE = "image"; 
 	ListView lvSimple;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState){
 		
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		
+		Bundle arguments = getIntent().getExtras();
+		if(arguments!=null){
+			o_dir= arguments.getString("dir");
+			NSDir= arguments.getString("file");}
+			else{o_dir= Environment.getExternalStorageDirectory().getPath().toString();
+			NSDir= null;}
+		dir= new File(o_dir+ NSDir)
+		files= dir.list();
 		TextView dir_tv = findViewById(R.id.nfold);
 		Button but_up = findViewById(R.id.up);
-		dir_tv.setText(root);
+		dir_tv.setText(o_dir);
 		int file_img=R.drawable.file;
 		int fold_img= R.drawable.folder;
 		int img;
@@ -51,45 +57,39 @@ public class MainActivity extends Activity
 		
         if (files != null)
 		{ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>( 
-				files.length); 
+			files.length); 
 			Map<String, Object> m; 
 			for (int i = 0; i < files.length; i++) { 
-				m = new HashMap<String, Object>(); 
-				m.put(ATTRIBUTE_NAME_TEXT, files[i]);
-				ndir= new File(root+ "/"+files[i]);
-				if (ndir.isDirectory()){
-					img= fold_img;}
-					else{img=file_img;}
-				m.put(ATTRIBUTE_NAME_IMAGE, img); 
-				data.add(m);
-				}
+			m = new HashMap<String, Object>(); 
+			m.put(ATTRIBUTE_NAME_TEXT, files[i]);
+			ndir= new File(o_dir + "/" + files[i]);
+			if (ndir.isDirectory()){
+				img= fold_img;}
+				else{img=file_img;}
+			m.put(ATTRIBUTE_NAME_IMAGE, img); 
+			data.add(m);}
 		
 		// массив имен атрибутов, из которых будут читаться данные 
 		String[] from = { ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_IMAGE }; 
 		// массив ID View-компонентов, в которые будут вставлять данные 
 		int[] to = { R.id.namef, R.id.ico }; 
-// создаем адаптер 
+		// создаем адаптер 
 		SimpleAdapter sAdapter = new SimpleAdapter(this, data,
 									R.layout.itemdir, from, to); 
-// определяем список и присваиваем ему адаптер 
+		// определяем список и присваиваем ему адаптер 
 		lvSimple = findViewById(R.id.lvSimple); 
 		lvSimple.setAdapter(sAdapter);}
 		lvSimple.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 				@Override
-				public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-				{
+				public void onItemClick(AdapterView<?> parent, View v, int position, long id){
 					// по позиции получаем выбранный элемент
 					NSDir = files[position];
-					newDir();
-				}
-			});
+					newDir();}
+		});
 	}
 				
-        
-		
-    
 public void newDir(){
-		Intent intent = new Intent(this, NewDir.class);
+		Intent intent = new Intent(this, MainActivity.class);
 		intent.putExtra("dir", dir.toString());
 		intent.putExtra("file", NSDir);
 		startActivity(intent); }
